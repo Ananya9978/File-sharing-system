361c95da4d2147e11e410321533f626d7024ee88
ananya9978 <ananyaom01@gmail.com>
2023-12-09 14:23:45 +0530

    added screenshots

--- fss/filesharing/views.py
+++ fss/filesharing/views.py
@@ -8,28 +8,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
 
 
-
-# Create your views here.
-
-# def download_file(request, file_id):
-#     uploaded_file = Document.objects.get(pk=file_id)
-#     response = HttpResponse(uploaded_file.file, content_type='application/force-download')
-#     response['Content-Disposition'] = f'attachment; filename="{uploaded_file.file.name}"'
-#     return response
-
 def signup(request):
     
     if request.method == 'POST':
-        print('in post')
         form = SignForm(request.POST,request.FILES)
         if form.is_valid():
-            print('is_valid')
             form.full_clean()
             form.save()
         else:
             print('error')
     
-    print('not post')
     form = SignForm()
     context = {'form' : form}
     return render(request, 'filesharing/signup.html', context)
@@ -37,20 +25,12 @@ def signup(request):
 def login(request):
     message = ''
     form = LoginForm()
-    print('in login')
-    u = UserDetails.objects.all()
-    for i in u:
-        print(i.email,i.password)
     if request.method == 'POST':
-        print('in post login')
         email = request.POST['email']
         password = request.POST['password']
-        print(email,password)
         resp = UserDetails.objects.filter(email=email,password=password).exists()
         user = authenticate(request, username = email, password = password)
         if resp == True:
-
-            # login(email)
             return redirect('index')
         else:
             message = 'Wrong username or password'
@@ -59,9 +39,7 @@ def login(request):
     return render(request, 'filesharing/login.html', context)
 
 
-def index(request):
-    print(request.user)
-        
+def index(request):        
     data = Document.objects.all()
     context = {'data' : data}
     return render(request, 'filesharing/index.html', context)
